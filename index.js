713083b5d6ce0ff7536cb2e390b7d4734c6d811b
Nguyen Thanh Binh <binh15122003@gmail.com>
2023-01-11 00:00:22 +0700

    update

--- index.js
+++ index.js
@@ -8,6 +8,26 @@ router.get("/", (req, res) => {
     massage: "ok",
   });
 });
+router.get("/users", (req, res) => {
+  return res.status(200).json({
+    massage: "ok",
+    data: [
+      {
+        id: 1,
+        name: "a",
+      },
+
+      {
+        id: 2,
+        name: "b",
+      },
+      {
+        id: 3,
+        name: "d",
+      },
+    ],
+  });
+});
 app.use("/", router);
 app.listen(port, () => {
   console.log("app listening at http://localhost:" + port);
